cb666b36b163b444775e220ebf34fd0e0e68a1d7
Rodrigo Farinango <farinango2328@saskpolytech.ca>
2023-11-24 12:42:23 -0600

    colocar clse de ruta activa

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,15 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { MarvelPage } from '../heroes/pages/MarvelPage'
-import { DcPage } from '../heroes/pages/DcPage'
-import { LoginPage } from '../auth/pages/LoginPage'
+
+import { Navbar } from '../ui'
+
+import { DcPage, MarvelPage } from '../heroes'
+import { LoginPage } from '../auth'
+
 
 export const AppRouter = () => {
   return (
     <>
+        <Navbar/>
         <Routes>
             <Route path='marvel' element={<MarvelPage/>}/>
             <Route path='dc' element={<DcPage/>}/>
