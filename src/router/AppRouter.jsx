784225b0d4dbe0ef77b44026c2045b88fd58aae9
Rodrigo Farinango <farinango2328@saskpolytech.ca>
2023-11-25 12:18:42 -0600

    Agregando un segundo router

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,16 @@
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
-import { Navbar } from '../ui'
-
-import { DcPage, MarvelPage } from '../heroes'
+import { HeroesRoutes } from '../heroes'
 import { LoginPage } from '../auth'
 
 
 export const AppRouter = () => {
   return (
-    <>
-        <Navbar/>
+    <>  
         <Routes>
-            <Route path='marvel' element={<MarvelPage/>}/>
-            <Route path='dc' element={<DcPage/>}/>
-            <Route path='login' element={<LoginPage/>}/>
+          <Route path='login' element={<LoginPage/>}/>
 
-            <Route path='/' element={<Navigate to="/marvel"/>}/>
+          <Route path='/*' element={<HeroesRoutes/>}/>
         </Routes>
     </>
   )
