247846fac0d58be49cbf3f33d8aa61cf16c4054e
Rodrigo Farinango <farinango2328@saskpolytech.ca>
2023-11-28 11:29:26 -0600

    tarjeta del heroe

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
+const CharactersByHero = ({alter_ego, characters}) => {
+    if(alter_ego === characters) return(<></>);
+
+    return <p>{characters}</p>;
+}
 
 export const HeroCard = ({
     id,
@@ -10,6 +17,9 @@ export const HeroCard = ({
 }) => {
 
     const heroImageUrl = `/assets/heroes/${id}.jpg`;
+
+    //const charactersByHero = (<p>{characters}</p>);
+
   return (
     <div
         className='col'
@@ -23,7 +33,20 @@ export const HeroCard = ({
                     <div className="card-body">
                         <h5 className='card-title'>{superhero}</h5>
                         <p className='card-text'>{alter_ego}</p>
-                        <p>{characters}</p>
+
+                        {/* {
+                            (alter_ego !== characters) && charactersByHero
+                        } */}
+
+                        <CharactersByHero characters={characters} alter_ego={alter_ego}/>
+                        <p className='card-text'>
+                            <small className='text-muted'>{first_appearance}</small>
+                        </p>
+                        
+                        <Link to={`/hero/${id}`}>
+                            Mas...
+                        </Link>
+                        
                     </div>
                 </div>
             </div>
