9fc173212722ff3c1e23767af5b344e8edf42c8d
Rodrigo Farinango <farinango2328@saskpolytech.ca>
2023-11-28 11:52:29 -0600

    leer argumentos por url

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,5 +1,20 @@
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers";
+
 export const HeroPage = () => {
+
+  const {id} = useParams();
+
+  const hero = getHeroById(id);
+
+  if(!hero){
+    return <Navigate to="/marvel"/>;
+  }
+
+  console.log(hero);
   return (
-    <h1>HeroPage</h1>
+    <>
+      <h1>{hero.superhero}</h1>
+    </>
   )
 }
--- src/heroes/routes/HeroesRoutes.jsx
+++ src/heroes/routes/HeroesRoutes.jsx
@@ -12,7 +12,7 @@ export const HeroesRoutes = () => {
                 <Route path='marvel' element={<MarvelPage/>}/>
                 <Route path='dc' element={<DcPage/>}/>
                 <Route path='search' element={<SearchPage/>}/>
-                <Route path='hero' element={<HeroPage/>}/>
+                <Route path='hero/:id' element={<HeroPage/>}/>
 
                 {/* Search, Hero by id */}
 
