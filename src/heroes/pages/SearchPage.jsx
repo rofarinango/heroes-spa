a8bcdc9fc483facb1e3d3ee48ea6eb53679fb0a4
Rodrigo Farinango <farinango2328@saskpolytech.ca>
2023-11-30 20:03:17 -0600

    mostrar listado de heroes

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -2,6 +2,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import queryString from 'query-string';
 import { useForm } from "../../hooks/useForm";
 import { HeroCard } from "../components";
+import { getHeroesByName } from "../helpers";
 
 export const SearchPage = () => {
 
@@ -9,11 +10,12 @@ export const SearchPage = () => {
   const location = useLocation();
 
   const {q = ''} = queryString.parse(location.search);
+  const heroes = getHeroesByName(q);
 
   console.log({location});
 
   const { searchText, onInputChange } = useForm({
-    searchText: '',
+    searchText: q,
 
   });
 
@@ -57,7 +59,12 @@ export const SearchPage = () => {
           <div className="alert alert-danger">
             No hero with <b>{q}</b>
           </div>
-          {/* <HeroCard/> */}
+          {
+            heroes.map(hero => (
+              <HeroCard key={hero.id} {...hero}/>
+            ))
+          }
+          {/*  */}
         </div>
       </div>
       
