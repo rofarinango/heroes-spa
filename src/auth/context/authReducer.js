de3ab707afd6393f4152d55356a26bed6dad1817
Rodrigo Farinango <farinango2328@saskpolytech.ca>
2023-12-01 14:49:18 -0600

    login de usuario

--- src/auth/context/authReducer.js
+++ src/auth/context/authReducer.js
@@ -4,13 +4,13 @@ const initialState = {
     logged: false,
 }
 
-export const authReducer = (state = initialState, action) => {
+export const authReducer = (state = {}, action) => {
     switch(action.type){
         case types.login:
             return {
                 ...state,
                 logged: true,
-                name: action.payload
+                user: action.payload
             };
         
         case types.logout:
--- src/ui/components/NavBar.jsx
+++ src/ui/components/NavBar.jsx
@@ -1,8 +1,12 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth';
 
 
 export const Navbar = () => {
 
+    const { user } = useContext(AuthContext);
+    console.log(user);
     const navigate = useNavigate();
 
     const onLogout = () => {
@@ -48,7 +52,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-primary'>
-                        Rodrigo
+                        {user?.name}
                     </span>
                     <button
                         className='nav-item nav-link btn'
