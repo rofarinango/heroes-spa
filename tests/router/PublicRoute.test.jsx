2d7481cff0b2db7d10c6a91fd3b96dca220dc5bd
Rodrigo Farinango <farinango2328@saskpolytech.ca>
2023-12-10 13:46:44 -0600

    pruebas en el public route

--- tests/router/PublicRoute.test.jsx
+++ tests/router/PublicRoute.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react"
 import { PublicRoute } from "../../src/router/PublicRoute"
 import { AuthContext } from "../../src/auth";
+import { MemoryRouter, Route, Routes } from "react-router-dom";
 
 describe('Pruebas en el <PublicRoute/>', () => { 
     test('debe de mostrar el children si no esta autenticado', () => {
@@ -22,6 +23,32 @@ describe('Pruebas en el <PublicRoute/>', () => {
      })
 
      test('debe de navegar si esta autenticado', () => { 
-        
+        const contextValue = {
+            logged: true,
+            user: {
+                name: 'Strider',
+                id: 'ABC123'
+            }
+        }
+        render(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={['/marvel']}>
+                    <Routes>
+                        <Route path='login' element={
+                            <PublicRoute>
+                                <h1>Ruta Publica</h1>
+                            </PublicRoute>
+                        }/>
+                        <Route path='marvel' element={<h1>Pagina Marvel</h1>}/>
+                    </Routes>
+                    
+                </MemoryRouter>
+                
+            </AuthContext.Provider>
+        );
+
+        expect(screen.getByText('Pagina Marvel')).toBeTruthy();
+
+        screen.debug();
       })
  })
